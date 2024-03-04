78bc3b5a013b115e49e5fa84a8f189af163c348f
Fredrick219 <154713981+Fredrick219@users.noreply.github.com>
2024-03-04 12:17:05 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+# AI-Powered Business Solutions
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+from PIL import Image
+
+
+import google.generativeai as genai
+
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(input,image,prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input,image[0],prompt])
+    return response.text
+    
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="AI-Powered Business Solutions")
+
+st.header("AI-Powered Business Solutions")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about the image")
+
+input_prompt = """
+               You are an expert business AI assistant and a business solutions provider in Customer Support Knowledge Base,
+               Compliance and Regulatory Guidance, Market Reasearch and Competitive Analysis, Legal Document Analysis and so on.
+               Please analyze the uploaded content and provide accurate assistance based on the user's query.
+               """
+
+## If ask button is clicked
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
